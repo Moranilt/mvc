4286da8c3d277a48894fde21cfecca4c3ee2bca5
Danil Berezhnoy <moranilt@yahoo.com>
2020-05-05 10:55:30 +0300

    Added autoload function, which autoloading each class from App directory

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php
-define("DIR", "app/");
-require DIR."model/model.php";
-require DIR."controller/controller.php";
-require DIR."view/view.php";
-require DIR."model/db.php";
+require_once "routes.php";
+function __autoload($class_name){
+  if(file_exists("./app/".$class_name.".php")){
+    require_once "./app/".$class_name.".php";
+  }else if(file_exists("./app/controller/".$class_name.".php")){
+    require_once "./app/controller/".$class_name.".php";
+  }else if(file_exists("./app/model/".$class_name.".php")){
+    require_once "./app/model/".$class_name.".php";
+  }else if(file_exists("./app/view/".$class_name.".php")){
+    require_once "./app/view/".$class_name.".php";
+  }
 
+}
 /*
 $model = new Model();
 $controller = new Controller($model);
 $view = new View($model, $template);
 */
-
+/*
 $template = "tpl/template.php";
 $db = new DB;
 $model = new Model($db);
 $view = new View($model, $template);
 $controller = new Controller($model);
 $controller->searchName("lily");
+*/
 
 
 
-echo $view->output();
+echo $_GET["url"];
  ?>
